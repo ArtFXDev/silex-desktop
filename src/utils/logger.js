8c12f42c39824f986584f966f17232e4f3db7553
Joseph HENRY <josephenry@protonmail.com>
2021-11-10 10:40:47 +0100

    feat: fix logs

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,16 +1,29 @@
 const pino = require("pino");
 const path = require("path");
+const os = require("os");
+const fs = require("fs");
 
-const logger = pino(
-  { prettyPrint: true },
-  process.env.NODE_ENV === "development"
-    ? undefined
-    : pino.destination(
-        path.join(
-          path.join(require("os").homedir(), "silex"),
-          ".silex_desktop_log"
-        )
-      )
-);
+// Creates the log dir if doesn't exist
+// Runs only once because node caches requires
+if (!fs.existsSync(process.env.SILEX_LOG_DIR)) {
+  fs.mkdirSync(process.env.SILEX_LOG_DIR, { recursive: true });
+}
+
+const transport = pino.transport({
+  target: "pino-pretty",
+  options: {
+    colorize: process.env.NODE_ENV === "development",
+    translateTime: true,
+    destination:
+      process.env.NODE_ENV === "development"
+        ? undefined
+        : path.join(
+            process.env.SILEX_LOG_DIR || path.join(os.homedir(), "silex"),
+            ".silex_desktop_log"
+          ),
+  },
+});
+
+const logger = pino(transport);
 
 module.exports = logger;
