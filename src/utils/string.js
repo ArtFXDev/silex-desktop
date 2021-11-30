eb43e35a89e2824d93d13e265d411b21358cde3b
Joseph HENRY <josephenry@protonmail.com>
2021-11-30 16:07:31 +0100

    refactor: replace var by let

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,9 +1,11 @@
 function asciiToHexa(str) {
-  var arr1 = [];
-  for (var n = 0, l = str.length; n < l; n++) {
+  const arr1 = [];
+
+  for (let n = 0; n < str.length; n++) {
     var hex = Number(str.charCodeAt(n)).toString(16);
     arr1.push(hex);
   }
+
   return arr1.join("");
 }
 
