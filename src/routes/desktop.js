0942d7fad0dbde7cf884e7aaa481cf22f84d96ad
Simon Lambin <slambin@artfx.fr>
2022-03-15 16:18:56 +0100

    Add current-processes depencency

---
+++ src/routes/desktop.js
@@ -0,0 +1,23 @@
+const store = require("./utils/store");
+const currentProcesses = require("current-processes");
+
+function register_status(app) {
+  app.get("/desktop/status", (req, res) => {
+    res.json(store.instance.data);
+  });
+}
+
+function register_processes(app) {
+  app.get("/desktop/processes", (req, res) => {
+    currentProcesses.get((err, processes) => {
+      res.json(processes);
+    });
+  });
+}
+
+function register_routes(app) {
+  register_status(app);
+  register_processes(app);
+}
+
+module.exports = register_routes;
